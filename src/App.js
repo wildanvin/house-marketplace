c7ff39bfd9c5297571e886d5995241d587245e2e
wildanvin <danvin.jim@gmail.com>
2022-04-14 02:02:47 -0500

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+//Pages
 import Explore from "./pages/Explore"
 import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
+
+//Components
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
@@ -19,8 +23,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   )
 }
