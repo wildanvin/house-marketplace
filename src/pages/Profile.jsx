e545f49ea05926f609b5389ceb7a51706be9d8ff
wildanvin <danvin.jim@gmail.com>
2022-04-14 12:06:44 -0500

    User SignIn

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,13 @@
+import { getAuth } from "firebase/auth"
+import { useState, useEffect } from "react"
+
 function Profile() {
-  return <div>Profile</div>
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+  return user ? <h1>{user.displayName}</h1> : "not logged in"
 }
 
 export default Profile
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 //import { toast } from 'react-toastify'
 import { Link, useNavigate } from "react-router-dom"
-//import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 //import OAuth from '../components/OAuth'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
@@ -23,25 +23,26 @@ function SignIn() {
     }))
   }
 
-  // const onSubmit = async (e) => {
-  //   e.preventDefault()
+  const onSubmit = async (e) => {
+    e.preventDefault()
 
-  //   try {
-  //     const auth = getAuth()
+    try {
+      const auth = getAuth()
 
-  //     const userCredential = await signInWithEmailAndPassword(
-  //       auth,
-  //       email,
-  //       password
-  //     )
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
 
-  //     if (userCredential.user) {
-  //       navigate('/')
-  //     }
-  //   } catch (error) {
-  //     toast.error('Bad User Credentials')
-  //   }
-  // }
+      if (userCredential.user) {
+        navigate("/")
+      }
+    } catch (error) {
+      console.log(error)
+      //toast.error('Bad User Credentials')
+    }
+  }
 
   return (
     <>
@@ -50,7 +51,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
