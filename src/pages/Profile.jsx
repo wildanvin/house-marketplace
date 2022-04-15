07d9407b0c8e736c178fdba51f495711aa83b122
wildanvin <danvin.jim@gmail.com>
2022-04-14 20:51:56 -0500

    User Logout Functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,33 @@
 import { getAuth } from "firebase/auth"
 import { useState, useEffect } from "react"
+import { useNavigate, LInk } from "react-router-dom"
 
 function Profile() {
-  const [user, setUser] = useState(null)
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
-  return user ? <h1>{user.displayName}</h1> : "not logged in"
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+
+  const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate("/")
+  }
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Log Out
+        </button>
+      </header>
+    </div>
+  )
 }
 
 export default Profile
