a2766d3fa378e9bdd2d7f27e436f5135f740f8d6
wildanvin <danvin.jim@gmail.com>
2022-04-17 02:20:15 -0500

    Fetch Linsting from Firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
+import Category from "./pages/Category"
 
 //Components
 import Navbar from "./components/Navbar"
@@ -22,6 +23,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+
           <Route path="profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
