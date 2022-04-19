a77c669c62d608c33de10545164e939899cd4262
wildanvin <danvin.jim@gmail.com>
2022-04-18 22:59:57 -0500

    Create Single Listing Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import Category from "./pages/Category"
 import CreateListing from "./pages/CreateListing"
+import Listing from "./pages/Listing"
 
 //Components
 import Navbar from "./components/Navbar"
@@ -33,6 +34,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categotyName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
