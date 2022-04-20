86d903538748f9e3e7024b754e6ca578066d4097
wildanvin <danvin.jim@gmail.com>
2022-04-20 01:27:20 -0500

    Edit Listing Page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Category from "./pages/Category"
 import CreateListing from "./pages/CreateListing"
 import Listing from "./pages/Listing"
 import Contact from "./pages/Contact"
+import EditListing from "./pages/EditListing"
 
 //Components
 import Navbar from "./components/Navbar"
@@ -35,6 +36,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/edit-listing/:listingId" element={<EditListing />} />
+
           <Route
             path="/category/:categotyName/:listingId"
             element={<Listing />}
--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
 import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg"
-// import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg"
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg"
 import bedIcon from "../assets/svg/bedIcon.svg"
 import bathtubIcon from "../assets/svg/bathtubIcon.svg"
 
-function ListingItem({ listing, id, onDelete }) {
+function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li className="categoryListing">
       <Link
@@ -56,7 +56,7 @@ function ListingItem({ listing, id, onDelete }) {
         />
       )}
 
-      {/* {onEdit && <EditIcon className="editIcon" onClick={() => onEdit(id)} />} */}
+      {onEdit && <EditIcon className="editIcon" onClick={() => onEdit(id)} />}
     </li>
   )
 }
