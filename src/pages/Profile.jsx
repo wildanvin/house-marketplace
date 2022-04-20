63de331de613df75511008a72b94f51747ce0136
wildanvin <danvin.jim@gmail.com>
2022-04-19 23:23:35 -0500

    Profile LIstings and Delete

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,27 @@
 import { useState, useEffect } from "react"
-
+import { Link } from "react-router-dom"
 import { getAuth, updateProfile } from "firebase/auth"
-import { updateDoc, doc } from "firebase/firestore"
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from "firebase/firestore"
 import { db } from "../firebase.config"
-import { useNavigate, Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
+import ListingItem from "../components/ListingItem"
 import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg"
 import homeIcon from "../assets/svg/homeIcon.svg"
 
 function Profile() {
   const auth = getAuth()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
   const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -20,6 +32,34 @@ function Profile() {
 
   const navigate = useNavigate()
 
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, "listings")
+
+      const q = query(
+        listingsRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      )
+
+      const querySnap = await getDocs(q)
+
+      let listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+
+    fetchUserListings()
+  }, [auth.currentUser.uid])
+
   const onLogout = () => {
     auth.signOut()
     navigate("/")
@@ -28,12 +68,12 @@ function Profile() {
   const onSubmit = async () => {
     try {
       if (auth.currentUser.displayName !== name) {
-        //Update display name in firebase
+        // Update display name in fb
         await updateProfile(auth.currentUser, {
           displayName: name,
         })
 
-        //Update in firestore
+        // Update in firestore
         const userRef = doc(db, "users", auth.currentUser.uid)
         await updateDoc(userRef, {
           name,
@@ -52,14 +92,28 @@ function Profile() {
     }))
   }
 
+  const onDelete = async (listingId) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingId))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId
+      )
+      setListings(updatedListings)
+      toast.success("Successfully deleted listing")
+    }
+  }
+
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return (
     <div className="profile">
       <header className="profileHeader">
         <p className="pageHeader">My Profile</p>
         <button type="button" className="logOut" onClick={onLogout}>
-          Log Out
+          Logout
         </button>
       </header>
+
       <main>
         <div className="profileDetailsHeader">
           <p className="profileDetailsText">Personal Details</p>
@@ -73,8 +127,9 @@ function Profile() {
             {changeDetails ? "done" : "change"}
           </p>
         </div>
+
         <div className="profileCard">
-          <form action="">
+          <form>
             <input
               type="text"
               id="name"
@@ -83,7 +138,6 @@ function Profile() {
               value={name}
               onChange={onChange}
             />
-
             <input
               type="text"
               id="email"
@@ -94,11 +148,29 @@ function Profile() {
             />
           </form>
         </div>
+
         <Link to="/create-listing" className="createListing">
           <img src={homeIcon} alt="home" />
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt="arrow right" />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your Listings</p>
+            <ul className="listingsList">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
